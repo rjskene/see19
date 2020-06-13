dcc05b4995406d5b65b15f2691b23466b742e5e7
Spindicate <rjskene83@gmail.com>
2020-06-13 19:42:35 -0400

    remove staticfiles app from settings

--- zooscraper/settings.py
+++ zooscraper/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'django_extensions',
 ]
 
