adf1fbe36f7943703bfb6a89e921a7335f7b60ad
Spindicate <rjskene83@gmail.com>
2020-06-18 13:03:15 -0400

    update

--- scripts/tweet_scheduler.py
+++ scripts/tweet_scheduler.py
@@ -2,6 +2,11 @@ from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from apscheduler.schedulers.blocking import BlockingScheduler
 from casestudy.covidcharts import tweetledee
 
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
+
 sched = BlockingScheduler(
     executors={
         'threadpool': ThreadPoolExecutor(max_workers=9),
@@ -9,8 +14,9 @@ sched = BlockingScheduler(
         }
 )
 
-@sched.scheduled_job('cron', hour=5, minute=59, second=45)
+@sched.scheduled_job('cron', hour=6, minute=10, second=10)
 def tweet_job():
+    print ('hello!!!!')
     tweetledee()
 
 def run():
